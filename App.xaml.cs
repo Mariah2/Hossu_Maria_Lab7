a132d6c4e11f59cc74a5e3df121f9055a366ab09
Mariah2 <maria.hossu@stud.ubbcluj.ro>
2022-12-01 16:14:31 +0200

    Lab8 done

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,28 @@
-﻿namespace Hossu_Maria_Lab7;
+﻿using System;
+using Nume_Pren_Lab7.Data;
+using System.IO;
+using Hossu_Maria_Lab7;
 
-public partial class App : Application
+namespace Hossu_Maria_Lab7;
+ public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-
-		MainPage = new AppShell();
-	}
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+    }
 }
